94939c82d42841276252b10600cf754dc49770a5
markomi <milicamarkovic@hotmail.com>
2020-02-05 16:39:05 +0100

    add module registry tests

--- tests/testthat/test-helper-fun.R
+++ tests/testthat/test-helper-fun.R
@@ -1,5 +1,6 @@
 context("Helper functions")
 
+## test mint header
 head_label <- "Header Title"
 head <-  get_header_mint(head_label)
 
@@ -7,4 +8,12 @@ test_that("Header check", {
   expect_equal(typeof(head), "list")
   expect_equal(class(head), "shiny.tag")
   expect_equal(head$children[[2]]$children[[1]], head_label)
+})
+
+## test module registry
+mod <- get_module_registry()
+
+test_that("Module registry", {
+  expect_equal(typeof(mod), "list")
+  expect_equal(length(mod), 5)
 })
