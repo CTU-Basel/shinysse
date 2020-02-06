ada0564fafaf18e84c50f6bc728130ee4f857f3b
markomi <milicamarkovic@hotmail.com>
2020-02-06 08:54:26 +0100

    tests added

--- tests/testthat/test-helper-fun.R
+++ tests/testthat/test-helper-fun.R
@@ -17,3 +17,40 @@ test_that("Module registry", {
   expect_equal(typeof(mod), "list")
   expect_equal(length(mod), 4)
 })
+
+## test calc_prop
+n <- seq(from = 1, to = 10, by = 1)
+theta <- seq(from = 0, to = 0.5, by = 0.1)
+xi <- seq(from = 0.1, to = 1, by = 0.1)
+alpha <- c(0.01, 0.025)
+save_path <- file.path("test_calc_prop.rds")
+suppressWarnings(suppressMessages(
+  calcs_prop <- calc_prop(n, theta, xi, alpha, save_path)
+))
+test_that("Power calc proportions", {
+  expect_equal(class(calcs_prop), "list")
+  expect_equal(length(calcs_prop), 2)
+  expect_equal(class(calcs_prop[[1]])[[1]], "powCalc")
+  expect_true(file.exists(save_path))
+})
+
+file.remove(save_path)
+
+## test calc_t
+n <- seq(from = 1, to = 10, by = 1)
+theta <- seq(from = 0, to = 10, by = 1)
+xi <- seq(from = 1, to = 10, by = 1)
+alpha <- c(0.01, 0.025)
+type <- "paired"
+save_path <- file.path("test_calc_t.rds")
+suppressWarnings(suppressMessages(
+  calcs_t <- calc_t(n, theta, xi, alpha, type, save_path)
+))
+test_that("Power calc proportions", {
+  expect_equal(class(calcs_prop), "list")
+  expect_equal(length(calcs_prop), 2)
+  expect_equal(class(calcs_prop[[1]])[[1]], "powCalc")
+  expect_true(file.exists(save_path))
+})
+
+file.remove(save_path)
